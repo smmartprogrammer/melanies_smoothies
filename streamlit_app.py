a5671c939184ce9500e40e01116884c0618c1dc2
Partab Raj <64382440+smmartprogrammer@users.noreply.github.com>
2025-10-27 14:03:33 +0500

    Update streamlit_app.py
    this line has been added
    from snowflake.snowpark import Session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+from snowflake.snowpark import Session
 
 
 # Set app title
