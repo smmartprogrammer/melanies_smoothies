0cc0dc39be11f61cfbb288ec055b4960a2bb0a39
Partab Raj <64382440+smmartprogrammer@users.noreply.github.com>
2025-10-28 10:01:56 +0500

    Update streamlit_app.py
    .json() has been added in st. text 6 line to have response in json

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 # Set app title
