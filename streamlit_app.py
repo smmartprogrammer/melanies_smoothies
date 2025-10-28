4ee7cc4b1abfa9598e817e9f8de9635858805a0e
Partab Raj <64382440+smmartprogrammer@users.noreply.github.com>
2025-10-28 10:08:24 +0500

    Update streamlit_app.py
    indentation update

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,9 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
 #new section to display nutritiant 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     
     time_to_insert = st.button('Submit Order')
